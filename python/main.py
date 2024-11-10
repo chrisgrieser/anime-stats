f17e14fe39af6af41a8db5c6fed4d7117a77b2ac
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-10 18:00:38 +0100

    feat: per genre stats

--- python/main.py
+++ python/main.py
@@ -11,37 +11,41 @@ import requests
 # ──────────────────────────────────────────────────────────────────────────────
 
 # CONFIG
-start_year = 2020
+start_year = 2014
 end_year = 2024
+genre_id = 62  # 62 = isekai, see https://api.jikan.moe/v4/genres/anime
+genre_name = "isekai"
 base_api = "https://api.jikan.moe/v4"
 
 
 # ──────────────────────────────────────────────────────────────────────────────
 
-request_count = 0  # share for all api calls
 
+def make_jikan_api_call(url: str) -> object:
+    """Make a jikan api call."""
+    # wait for rate limit, 3 calls per second https://docs.api.jikan.moe/#section/Information/Rate-Limiting
+    time.sleep(1)
 
-def wait_for_api_rate_limit() -> None:
-    """Wait for api rate limit."""
-    calls_per_second = 3 # https://docs.api.jikan.moe/#section/Information/Rate-Limiting
-    global request_count  # noqa: PLW0603 too convoluted without `global`
-    request_count += 1
-    if request_count > calls_per_second:
-        request_count = 0
-        time.sleep(1.5)
+    # make request
+    response = requests.get(url, timeout=10)
+    http_status_success = 200
+    if response.status_code != http_status_success:
+        print("Error:", response.status_code, response.reason)
+        sys.exit(1)
+    return response.json()  # pyright: ignore [reportAny]
 
 
 class YearData(TypedDict):
     """Data for a year."""
 
     total: int
-    isekais: int
+    of_genre: int
 
 
 # ──────────────────────────────────────────────────────────────────────────────
 
 
-def get_data_per_year() -> dict[int, YearData]:
+def get_data_per_year(genre_id: int) -> dict[int, YearData]:
     """Get the total number of shows per year.
 
     DOCS https://docs.api.jikan.moe/#tag/anime/operation/getAnimeSearch
@@ -49,28 +53,28 @@ def get_data_per_year() -> dict[int, YearData]:
     year_data: dict[int, YearData] = {}
 
     for year in range(start_year, end_year + 1):
-        wait_for_api_rate_limit()
         api_url = f"{base_api}/anime?start_date={year}-01-01&end_date={year}-12-31&type=tv"
-        response = requests.get(api_url, timeout=10)
-        http_status_success = 200
-        if response.status_code != http_status_success:
-            print("Error:", response.status_code, response.reason)
-            sys.exit(1)
+        total_for_year = make_jikan_api_call(api_url)
 
-        pagination = response.json()["pagination"]  # pyright: ignore [reportAny]
-        if year not in year_data:
-            year_data[year] = {"total": 0, "isekais": 0}
+        api_url += api_url + f"&genres={genre_id}"
+        of_genre_for_year = make_jikan_api_call(api_url)
 
-        year_data[year]["total"] = pagination["items"]["total"]
+        year_data[year] = {
+            "of_genre": of_genre_for_year["pagination"]["items"]["total"],  # pyright: ignore [reportIndexIssue]
+            "total": total_for_year["pagination"]["items"]["total"],  # pyright: ignore [reportIndexIssue]
+        }
 
     return year_data
 
 
 # ──────────────────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    year_data = get_data_per_year()
+    year_data = get_data_per_year(genre_id)
 
-    to_print = ""
+    to_print: list[str] = [(f"{genre_name}s per year").upper()]
     for year, data in year_data.items():
-        to_print += f"{year}: {data["total"]}\n"
-    print(to_print)
+        of_genre, total = data["of_genre"], data["total"]
+        share = round((of_genre / total) * 100)
+        to_print.append(f"{year}: {of_genre}/{total} ({share}%)")
+
+    print("\n".join(to_print))
