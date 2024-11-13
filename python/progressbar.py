86877cbe4fd22658b26aa85c36aa29a01f474ada
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-13 16:44:52 +0100

    refactor: progress bar

--- python/progressbar.py
+++ python/progressbar.py
@@ -1,20 +1,32 @@
 """Simple progress bar for the terminal."""
 
-EMPTY_CHAR = "─"
-CHAR = "■"
-FROM_CACHE = "□"
+from typing import TypedDict
+
+CHARS = {
+    "empty": "─",
+    "load": "■",
+    "load_from_cache": "□",
+}
 
 
-progressbar_len = 0
-previous_progress = ""
+class BarState(TypedDict):
+    """Simple state management for the progress bar."""
+
+    bar_len: int  # total length the bar will have
+    previous: str  # previously printed chars
+
+
+state: BarState = {
+    "bar_len": 0,
+    "previous": "",
+}
 
 # ──────────────────────────────────────────────────────────────────────────────
 
 
 def init(length: int) -> None:
     """Define the length of the progress bar."""
-    global progressbar_len  # noqa: PLW0603
-    progressbar_len = length
+    state["bar_len"] = length
 
 
 def increment(*, from_cache: bool = False) -> None:
@@ -22,12 +34,14 @@ def increment(*, from_cache: bool = False) -> None:
 
     Use `from_cache=True` to use an alternate char.
     """
-    global previous_progress  # noqa: PLW0603
-    previous_progress += CHAR if not from_cache else FROM_CACHE
-    filled_bar = previous_progress + (progressbar_len - len(previous_progress)) * EMPTY_CHAR
+    bar = state["previous"] + (CHARS["load"] if not from_cache else CHARS["load_from_cache"])
+    filled_bar = bar + (state["bar_len"] - len(bar)) * CHARS["empty"]
     print("\r" + filled_bar, end="", flush=True)
+    state["previous"] = bar
 
 
 def remove() -> None:
-    """Remove the progress bar."""
+    """Remove the progress bar and reset the state."""
     print("\r" + " " * 100 + "\r", end="", flush=True)
+    state["previous"] = ""
+    state["bar_len"] = 0
